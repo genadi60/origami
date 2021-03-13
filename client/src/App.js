befbeea9bc8492514eaa5bdf14656b7dcc5301b9
genadi60 <genadimihaylov@gmail.com>
2021-03-14 00:29:40 +0200

    Implement footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,13 @@
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets begin</h1>
-      <nav className="Navigation">
-        
-        <ul>
-          <li className="Link_listItem"><a href="/"><img src="/white-origami-bird.png" alt="origami bird"></img></a></li>
-          <li className="Link_listItem"><a href="#">Going to 1</a></li>
-          <li className="Link_listItem"><a href="#">Going to 2</a></li>
-          <li className="Link_listItem"><a href="#">Going to 3</a></li>
-          <li className="Link_listItem"><a href="#">Going to 4</a></li>
-          <li className="Link_listItem"><a href="#">Going to 5</a></li>
-          <li className="Link_listItem"><a href="#">Going to 6</a></li>
-          <li className="Link_listItem"><a href="#">Going to 7</a></li>
-          <li className="Link_listItem"><a href="#">Going to 8</a></li>
-          <li className="Link_listItem"><a href="#">Going to 9</a></li>
-          <li className="Link_listItem"><a href="#">Going to 10</a></li>
-          <li className="Link_listItem"><a href="#">Going to 11</a></li>
-        </ul>
-      </nav>
+      
+      <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,25 @@
+import './Footer.css';
+import ListItem from '../ListItem';
+const Footer = () => {
+    return (
+        <footer class="Footer">
+            <ul>
+                <ListItem> Going to 1</ListItem>
+                <ListItem> Going to 2</ListItem>
+                <ListItem> Going to 3</ListItem>
+                <ListItem> Going to 4</ListItem>
+                <ListItem> Going to 5</ListItem>
+                <ListItem> Going to 6</ListItem>
+                <ListItem> Going to 7</ListItem>
+                <ListItem> Going to 8</ListItem>
+                <ListItem> Going to 9</ListItem>
+                <ListItem> Going to 10</ListItem>
+                <ListItem> Going to 11</ListItem>
+                <li className="ListItem"><a href="/"><img src="/blue-origami-bird-flipped.png" alt="origami bird"></img></a></li>
+            </ul>
+            <p>Software University &copy; 2021</p>
+        </footer>
+    )
+};
+
+export default Footer;
